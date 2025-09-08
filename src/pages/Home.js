d67d6d9c166eddb280bd5791a11e7202677269f0
Krupali0609 <Krupali0609@users.noreply.github.com>
2025-09-07 23:59:37 -0400

    Created home page without navigation tabs

---
+++ src/pages/Home.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div className="home-container"> 
+      <div className="home-box">
+
+        {/* Title */}
+        <h1 className="home-title">Welcome to Group Expense Tracker</h1>
+
+        {/* Two-column layout */}
+        <div className="home-content">
+          {/* Left side - Image */}
+          <div className="home-left">
+            <img 
+              src="https://www.shutterstock.com/shutterstock/photos/2382150379/display_1500/stock-vector-finance-control-hand-drawn-composition-expense-tracker-in-mobile-bank-account-smartphone-app-with-2382150379.jpg" 
+              alt="Sample"
+              className="home-image"
+            />
+          </div>
+
+          {/* Right side - Paragraph & Button */}
+          <div className="home-right">
+            <p className="home-description">
+              This is a simple React application with a modern blue theme.
+              Explore more features by clicking the button below!
+            </p>
+            <button className="home-button">Get Started</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
